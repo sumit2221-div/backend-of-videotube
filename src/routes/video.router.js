8657101570387855d32d6e0a11c4228a96372b25
sumit212-dev <132038818+sumit2221-div@users.noreply.github.com>
2024-06-15 12:31:22 +0530

    some chhanges

--- src/routes/video.router.js
+++ src/routes/video.router.js
@@ -7,25 +7,30 @@ import {
     deleteVideo,
     togglePublishStatus,
     Getsubscribervideos
-} from "../contorller/video.controller.js";
-import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { upload } from "../middlewares/multer.middleware.js";
+}
+ from "../contorller/video.controller.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
+import {upload} from "../middlewares/multer.middleware.js"
 
 const router = Router();
+
 
 router
     .route("/")
     .get(getAllVideos)
     .post(
-        verifyJWT, // Apply verifyJWT middleware to this route
+        verifyJWT,
         upload.fields([
             {
                 name: "videofile"
-            }, 
-            {
+            
+        
+            }, {
                 name: "thumbnail",
-                maxCount: 1,
-            }
+                maxCount : 1, 
+                
+            },
+            
         ]),
         publishAVideo
     );
@@ -33,10 +38,10 @@ router
 router
     .route("/:videoId")
     .get(getVideoById)
-    .delete(verifyJWT, deleteVideo) // Apply verifyJWT middleware to this route
-    .patch(verifyJWT, upload.single("thumbnail"), updateVideo); // Apply verifyJWT middleware to this route
+    .delete(verifyJWT,deleteVideo)
+    .patch(upload.single("thumbnail"),verifyJWT, updateVideo);
 
-router.route("/toggle/publish/:videoId").patch(verifyJWT, togglePublishStatus); // Apply verifyJWT middleware to this route
-router.route("/v/subscriber").get(verifyJWT, Getsubscribervideos); // Apply verifyJWT middleware to this route
+router.route("/toggle/publish/:videoId").patch(togglePublishStatus);
+router.route("/v/subscriber").get(verifyJWT,Getsubscribervideos)
 
-export default router;
+export default router
